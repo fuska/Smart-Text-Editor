f5cb693dcd756b26f67d48c3252f697b982f3642
fuska <rooober@gmail.com>
2023-05-19 08:38:17 +0200

    Update Workspace2.js

--- dist/Workspace2.js
+++ dist/Workspace2.js
@@ -93,8 +93,6 @@ export function createWindow() {
     const features = (STE.appearance.standalone || STE.appearance.fullscreen) ? "popup" : "", win = window.open(window.location.href, "_blank", features);
     //const features = (STE.appearance.standalone || STE.appearance.fullscreen) ? "popup" : "", win = window.open(window.location.href, , features);
     alert("hola")
-    if (!confirm(`comprobar si se ejecuta en la versión desktop.`))
-                return;
      
 
     if (win === null)
